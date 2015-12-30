90abbe076803ad2a3fc3c00db9a89fd50ce18ed0
nick <nick.payne89@gmail.com>
2015-12-30 14:50:39 +0100

    databinding example

--- example.js
+++ example.js
@@ -1,19 +1,29 @@
-var ExampleApplication = React.createClass({
-  render: function() {
-    var elapsed = Math.round(this.props.elapsed  / 100);
-    var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-    var message =
-      'React has been successfully running for ' + seconds + ' seconds.';
-
-    return <p>{message}</p>;
-  }
+var DataBindingForm = React.createClass({
+    getInitialState: function() {
+        return {value: 'ReactJS'};
+    },
+    handleValueChange: function(e) {
+        this.setState({value: e.target.value});
+    },
+    render: function() {
+        return (
+            <form className="dataBindingForm">
+                <label>Hello</label>
+                <input
+                    type="text"
+                    placeholder="Databinding"
+                    value={this.state.value}
+                    onChange={this.handleValueChange}
+                />
+                <br />
+                <p>{ this.state.value }</p>
+            </form>
+        );
+    }
 });
 
-var start = new Date().getTime();
-
-setInterval(function() {
-  ReactDOM.render(
-    <ExampleApplication elapsed={new Date().getTime() - start} />,
+ReactDOM.render(
+    <DataBindingForm />,
     document.getElementById('container')
-  );
-}, 50);
+);
+
